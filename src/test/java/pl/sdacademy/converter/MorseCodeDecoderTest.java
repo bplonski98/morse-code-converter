58868e677d941e51688ebc9f0824157fef0da22c
Bartłomiej Płoński <bartek98103@gmail.com>
2023-03-22 18:04:43 +0100

    Task2 - MorseCodeEncoder and MorseCodeDecoder logic and tests added

---
+++ src/test/java/pl/sdacademy/converter/MorseCodeDecoderTest.java
@@ -0,0 +1,51 @@
+package pl.sdacademy.converter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MorseCodeDecoderTest {
+
+    private final MorseCodeDictionary dictionary = new MorseCodeDictionary();
+    private final MorseCodeDecoder decoder = new MorseCodeDecoder(dictionary);
+
+    public static Stream<Arguments> paramsProvider() {
+        return Stream.of(
+                //  String input, String expectedCode
+                Arguments.of("SDA", "... -.. .-"),
+                Arguments.of("SOS", "... --- ..."),
+                Arguments.of("JAVA", ".--- .- ...- .-"),
+                Arguments.of("HEY SDA", ".... . -.--   ... -.. .-"),
+                Arguments.of("JAVA ROCKS", ".--- .- ...- .-   .-. --- -.-. -.- ..."),
+                Arguments.of("I LOVE JAVA", "..   .-.. --- ...- .   .--- .- ...- .-"),
+                Arguments.of("SOFTWARE DEVELOPMENT ACADEMY", "... --- ..-. - .-- .- .-. .   -.. . ...- . .-.. --- .--. -- . -. -   .- -.-. .- -.. . -- -.--")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramsProvider")
+    void testDecodeHappyPath(String expectedText, String input) {
+
+        // when
+        final String actualText = decoder.decode(input);
+
+        // then
+        Assertions.assertEquals(expectedText, actualText);
+    }
+    @ParameterizedTest
+    @MethodSource("paramsProvider")
+    void testDecodeStreamMethodHappyPath(String expectedText, String input) {
+
+        // when
+        final String actualText = decoder.decodeStreamMethod(input);
+
+        // then
+        Assertions.assertEquals(expectedText, actualText);
+    }
+}
